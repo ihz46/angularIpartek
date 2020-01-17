b362c4b65f4a47b90ca98117215d3ba5dfbb1702
inigo hernandez <ihernandezzarraga@gmail.com>
2020-01-17 12:42:22 +0100

    producto con card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,4 +20,88 @@ export class AppComponent {
 
   //Variable
   title = 'Welcome to THE HELL';
+
+  //Creamos un array con un JSON
+  productos = [{
+    "id": 34,
+    "nombre": "café",
+    "gluten": "false",
+    "precio": 1.23,
+    "imagen": "https://as01.epimg.net/deporteyvida/imagenes/2018/11/30/portada/1543604759_559830_1543604909_noticia_normal.jpg",
+    "categoria": {
+      "id": 1,
+      "nombre": "bebidas"
+    },
+    "historico": [{
+      "fecha": "01/12/2019",
+      "precio": 1.50
+    },
+    {
+      "fecha": "03/12/2019",
+      "precio": 2.20
+    },
+    {
+      "fecha": "12/12/2019",
+      "precio": 2.90
+    }
+    ]
+  },
+
+  {
+    "id": 5,
+    "nombre": "aceitunas",
+    "gluten": "false",
+    "precio": 3.50,
+    "imagen": "https://faroliva.com/450-thickbox_default/aceitunas-rellenas-de-anchoa-gigante.jpg",
+    "categoria": {
+      "id": 2,
+      "nombre": "conservas"
+    },
+    "historico": [{
+      "fecha": "01/12/2019",
+      "precio": 5.60
+    },
+    {
+      "fecha": "03/12/2019",
+      "precio": 7.40
+    },
+    {
+      "fecha": "12/12/2019",
+      "precio": 10
+    }
+    ]
+  },
+  {
+    "id": 2,
+    "nombre": "pan",
+    "gluten": "true",
+    "precio": 1,
+    "imagen": "https://www.recetasdepan.net/wp-content/uploads/2019/06/Receta-de-pan-de-masa-madre-de-San-Francisco.jpg",
+    "categoria": {
+      "id": 3,
+      "nombre": "panaderia"
+    },
+    "historico": [{
+      "fecha": "01/12/2019",
+      "precio": 1.40
+    },
+    {
+      "fecha": "03/12/2019",
+      "precio": 2.80
+    },
+    {
+      "fecha": "12/12/2019",
+      "precio": 2.24
+    }
+    ]
+  }
+  ]
+
+  pSeleccionado = this.productos[0];
+
+  seleccionarProducto = function (producto) {
+    console.log('hemos hecho click %o', producto);
+    this.pSeleccionado = producto;
+  }
+
 }
