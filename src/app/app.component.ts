f7b689c4b0313f1d1037d93cfecdb556e1647cf6
inigo hernandez <ihernandezzarraga@gmail.com>
2020-01-17 09:43:28 +0100

    app angular inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+/*
+ * Componente principal que se carga al arrancar la aplicación
+ * 
+ * selector: nombre de la etiqueta para inyectar el componente.
+ * 
+ * templateUrl: indica la ruta donde se encuentra la vista en HTML.
+ * 
+ * styleUrls: indica donde se encuentra el estilo para la vista en formato SCSS.
+ * 
+ */
+
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //nombre de la etiqueta para inyectar el componente
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularApp';
+
+  //Variable
+  title = 'Welcome to THE HELL';
 }
