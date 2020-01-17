a13ad4c85456bbd5ae0068eab0f7b8351fcc406e
inigo hernandez <ihernandezzarraga@gmail.com>
2020-01-17 13:02:31 +0100

    commit para subir a mi repositorio de github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BotonComponent } from './componentes/boton/boton.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BotonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/boton/boton.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boton',
+  templateUrl: './boton.component.html',
+  styleUrls: ['./boton.component.scss']
+})
+export class BotonComponent implements OnInit {
+
+  //Declaramos variables
+  numero: number;
+
+  constructor() {
+    //Iniciamos variables
+    console.trace('BotonComponent constructor');
+    this.numero = 0;
+  }
+
+  ngOnInit() {
+
+  }
+
+  //funciones
+  contadorBoton = function () {
+
+  }
+
+}
