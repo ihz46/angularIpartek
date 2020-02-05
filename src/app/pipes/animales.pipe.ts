cdf3da99b9c0486db3668f1d918219a8ba26ba0f
inigo hernandez <ihernandezzarraga@gmail.com>
2020-02-05 14:01:04 +0100

    pequeños cambios

--- src/app/pipes/animales.pipe.ts
+++ src/app/pipes/animales.pipe.ts
@@ -5,17 +5,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class AnimalesPipe implements PipeTransform {
 
-  transform(datos: any, busqueda: string): any {
+  transform(datos: any, busqueda: string, tipo: string): any {
 
     console.debug(datos);
     console.debug(busqueda);
 
+    console.debug(tipo);
+
+    tipo = tipo.toUpperCase();
     busqueda = busqueda.toUpperCase();
 
     const resultado = datos.filter((el) => {
       console.debug(el);
       const nombre = el.Nombre.toUpperCase();
+      const tipoAnimal = el.Tipo.toUpperCase();
       return nombre.includes(busqueda);
+
     });
 
     return resultado;
